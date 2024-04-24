32ab480a31760d4c2816be33817a6cb3c85ef512
SparkFlowGt <scinteiestefan@protonmail.com>
2024-04-24 12:24:24 +0300

    derived

--- src/App.js
+++ src/App.js
@@ -7,12 +7,35 @@ const initialItems = [
 ];
 
 export default function App() {
+  const [items, setItems] = useState(initialItems);
+  const numItems = items.lenght;
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems(
+      (items = items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      ))
+    );
+  }
+
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
-      <Stats />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItems={handleToggleItem}
+      />
+      <Stats items={items} />
     </div>
   );
 }
@@ -20,9 +43,9 @@ export default function App() {
 function Logo() {
   return <h1>🌴 Far away 👜</h1>;
 }
-function Form() {
+function Form({ onAddItems }) {
   const [description, setDescription] = useState("");
-  const [quantity, setQuantity] = useState(1);
+  const [quantity, setQuantity] = useState();
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -30,6 +53,8 @@ function Form() {
     if (!description) return;
 
     const newItem = { description, quantity, packed: false, id: Date.now() };
+
+    onAddItems(newItem);
 
     setDescription("");
     setQuantity(1);
@@ -58,34 +83,54 @@ function Form() {
     </form>
   );
 }
-function PackingList() {
+function PackingList({ items, onDeleteItem, handleToggleItem }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
-          <Item item={item} key={item.id} />
+        {items.map((item) => (
+          <Item item={item} onDeleteItem={onDeleteItem} key={item.id} />
         ))}
       </ul>
     </div>
   );
 }
 
-function Item({ item }) {
+function Item({ item, onDeleteItem, onToggleItem }) {
   return (
     <li>
+      <input
+        type="checkbox"
+        value={item.packed}
+        onChange={() => onToggleItem(item.id)}
+      />
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity}
         {item.description}
       </span>
-      <button>delete</button>
+      <button onClick={() => onDeleteItem(item.id)}>delete</button>
     </li>
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.lenght)
+    return (
+      <p className="footer">
+        <em>Start adding some items to your packing list</em>
+      </p>
+    );
+
+  const numItems = items.lenght;
+  const numPacked = items.filter((item) => item.packed).lenght;
+  const percentage = Math.round((numPacked / numItems) * 100);
+
   return (
     <footer className="stats">
-      <em>👜 You have a X items on your list, and you already packed X (x%)</em>
+      <em>
+        {percentage === 100
+          ? " You got everithing ready to go"
+          : `👜 You have a ${numItems} items on your list, and you already packed X ${numPacked}`}
+      </em>
     </footer>
   );
 }
